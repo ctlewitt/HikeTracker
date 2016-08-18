21eb88ccf0dbfcaf853b2ed80086346dc19c2167
Charley Lewittes <ctlewitt@gmail.com>
2016-08-17 23:59:51 -0400

    debugging getting location from ip

--- hiketracker/utils.py
+++ hiketracker/utils.py
@@ -7,7 +7,7 @@ import random
 def get_ip_addr(request):
     try:
 
-        return request.headers["X_FORWARDED_FOR"].split(",")[1].strip()
+        return request.headers["X_FORWARDED_FOR"].split(",")[0].strip()
     except KeyError:
         try:
             return "8.8.8.8"
